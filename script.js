00236964b9bdcf9c9442336757932a0ea655f489
islafranco <isla_franco@icloud.com>
2022-06-07 19:58:33 -0700

    getting data works in console

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-// console.log("I love pizza");
-// const url = ""
 
-// fetch(url)
-//     .then(res => res.json())
-//     .then(data => {
-//         console.log(data)
-//     })
-//     .catch(err => console.log(err))
-const URL = 'https://aztro.sameerkumar.website/?sign=leo&day=today';
+const URL = 'https://aztro.sameerkumar.website/?sign=leo&day=today'
 
-fetch(URL, {
+const form = document.querySelector('#searchBar');
+const sign= ['sagittarius', 'aquarius', 'capricorn', 'aries', 'libra', 'virgo',
+'scorpio', 'taurus', 'pisces', 'gemini', 'leo', 'cancer'];
+
+function getData (event) {
+    event.preventDefault();
+    let textInput = document.querySelector('#inputBar')
+    let signName = document.querySelector('#sign-name')
+
+    console.log(textInput)
+}
+
+ fetch(URL, {
     method: 'POST'
 })
 .then(res => res.json())
 .then(json => {
     const date = json.current_date;
-    console.log(date)
+    const num = json.lucky_number;
+    const horoscope = json.description;
+    console.log(date, num, horoscope)
 });
+
+form.addEventListener("submit", getData)
+
+
