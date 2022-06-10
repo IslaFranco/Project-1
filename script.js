06b4301f45cc09a0fc158b94b7b9b3c11951852d
islafranco <isla_franco@icloud.com>
2022-06-09 22:30:15 -0700

    js needs work

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // const baseUrl = "https://aztro.sameerkumar.website"
 
 
- let sign = ['sagittarius', 'aquarius', 'capricorn', 'aries', 'libra', 'virgo',
-'scorpio', 'taurus', 'pisces', 'gemini', 'leo', 'cancer'];
-let day = ['today', 'yesterday', 'tomorrow'];
+//  let sign = ['sagittarius', 'aquarius', 'capricorn', 'aries', 'libra', 'virgo',
+// 'scorpio', 'taurus', 'pisces', 'gemini', 'leo', 'cancer'];
+// let day = ['today', 'yesterday', 'tomorrow'];
 
-const URL = 'https://aztro.sameerkumar.website?sign=leo&day=today'
+const URL = `https://aztro.sameerkumar.website?sign=LEO&day=today`
 
 
 const form = document.querySelector('#search');
@@ -13,18 +13,19 @@ const form = document.querySelector('#search');
 
 function getData (event) {
     event.preventDefault();
-    let textInput = document.querySelector('#sign-search').value;
+    let options = document.querySelector('#virgo').value;
     let signName = document.querySelector('#sign-name')
     let horoscope = document.querySelector("#description")
 
-    console.log(textInput)
+    console.log(options)
 
 
- fetch(URL, {
-    method: 'POST'
-})
+ fetch(`https://aztro.sameerkumar.website?sign=${options}&day=today`, {
+     method: 'POST'
+ })
 .then(res => res.json())
 .then(data => console.log(data))
+.catch(err => console.log(err))
 }
 
 form.addEventListener("submit", getData)
