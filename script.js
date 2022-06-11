3303688c3585380f4e25f5a6a87dd10e1c39fb1b
islafranco <isla_franco@icloud.com>
2022-06-11 14:21:07 -0700

    more css

--- script.js
+++ script.js
@@ -5,6 +5,17 @@
 // 'scorpio', 'taurus', 'pisces', 'gemini', 'leo', 'cancer'];
 // let day = ['today', 'yesterday', 'tomorrow'];
 
+let scrollToTopBtn = document.getElementById("scrollToTopBtn")
+let rootElement = document.documentElement
+
+function scrollToTop() {
+    rootElement.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+  }
+  scrollToTopBtn.addEventListener("click", scrollToTop)
+
 const URL = `https://aztro.sameerkumar.website?sign=LEO&day=today`
 
 
